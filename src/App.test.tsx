f4243423ce0bf4a7b86b236c735a2847c5de3df1
SL-23 <stephanieluvsphy@gmail.com>
2022-03-27 20:19:20 +1030

    add unit test for business logic

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import store from "./common/state/store";
+import { LoadInitialData, addSave, removeSave } from "./common/state/PropertyListActions";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { waitFor } from "@testing-library/react";
+
+it("load initial property list", async () => {
+  await waitFor(() => store.dispatch(LoadInitialData()));
+  const initialListState = store.getState().propertyList;
+  // expect to load all 4 properties
+  expect(initialListState.empty).toBe(false);
+  expect(initialListState.ready).toBe(true);
+  expect(Object.keys(initialListState.propertyList).length).toBe(4);
+})
+
+it("update the save flag to save a property", async () => {
+  await waitFor(() => store.dispatch(addSave("1")));
+
+  const listState = store.getState().propertyList;
+  // expect: should still be 4 properties
+  expect(listState.empty).toBe(false);
+  expect(listState.ready).toBe(true);
+  expect(Object.keys(listState.propertyList).length).toBe(4);
+  const target = listState.propertyList["1"];
+  // save flag should be true
+  expect(target.saved).toBe(true);
+})
+
+it("update the save flag to remove a property", async () => {
+  await waitFor(() => store.dispatch(removeSave("4")));
+  const listState = store.getState().propertyList;
+  // expect: should still be 4 properties
+  expect(listState.empty).toBe(false);
+  expect(listState.ready).toBe(true);
+  expect(Object.keys(listState.propertyList).length).toBe(4);
+  const target = listState.propertyList["4"];
+  // save flag should be flase
+  expect(target.saved).toBe(false);
+})
